03dcc38db2e5d3e9e37978c30ded6c3b20986ce4
ahmedoo124 <121548297+ahmedoo124@users.noreply.github.com>
2022-12-28 10:08:44 +0000

    Add files via upload

---
+++ ahmed.cs
@@ -0,0 +1,15 @@
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        } 
+    }
+}
